b29325a8731c001415ee9efdd3a8b89d3f6c0387
Cassio Filho <cassioaafilho@gmail.com>
2023-02-20 10:57:55 -0300

    Add likes

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,6 +3,7 @@ import { Course } from './Course'
 import { Episode } from './Episode'
 import { Favorite } from './Favorite'
 import { User } from './User'
+import { Like } from './Like'
 
 Category.hasMany(Course, { as: 'courses' })
 
@@ -10,6 +11,7 @@ Course.belongsTo(Category)
 Course.hasMany(Episode, { as: 'episodes' })
 Course.belongsToMany(User, { through: Favorite })
 Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
+Course.belongsToMany(User, { through: Like })
 
 Episode.belongsTo(Course)
 
@@ -17,6 +19,7 @@ Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite })
+User.belongsToMany(Course, { through: Like })
 User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
 
 export {
@@ -24,5 +27,6 @@ export {
   Course,
   Episode,
   User,
-  Favorite
+  Favorite,
+  Like
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { episodesController } from './controllers/episodeController'
 import { favoritesController } from './controllers/favoritesController'
 import { authController } from './controllers/authController'
 import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
+import { likesController } from './controllers/likeController'
 
 const router = express.Router()
 
@@ -14,7 +15,6 @@ router.post('/auth/login', authController.login)
 router.get('/categories', ensureAuth, categoriesController.index)
 router.get('/categories/:id', ensureAuth, categoriesController.show)
 
-
 router.get('/courses/featured', ensureAuth, coursesController.featured)
 router.get('/courses/newest', ensureAuth, coursesController.newest)
 router.get('/courses/search', ensureAuth, coursesController.search)
@@ -23,6 +23,9 @@ router.get('/courses/:id', ensureAuth, coursesController.show)
 router.get('/favorites', ensureAuth, favoritesController.index)
 router.post('/favorites', ensureAuth, favoritesController.save)
 router.delete('/favorites', ensureAuth, favoritesController.delete)
+
+router.post('/likes', ensureAuth, likesController.save)
+router.delete('/likes', ensureAuth, likesController.delete)
 
 router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
 
