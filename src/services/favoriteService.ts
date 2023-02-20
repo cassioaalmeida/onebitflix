9f1a1125dc2170faed3f317acef1a4a1ed95e959
Cassio Filho <cassioaafilho@gmail.com>
2023-02-20 11:00:04 -0300

    Add like and favorite into courses

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -36,5 +36,16 @@ export const favoriteService = {
         courseId
       }
     })
+  },
+  
+  isFavorited: async (userId: number, courseId: number) => {
+    const favorite = await Favorite.findOne({
+      where: {
+        userId,
+        courseId
+      }
+    })
+
+    return favorite !== null
   }
 }
